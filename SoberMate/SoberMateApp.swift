9b50cb41f3db5c138b64a2c5f8c6af9827aaf239
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-07-04 14:41:44 +0900

    Initial Commit

---
+++ SoberMate/SoberMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoberMateApp.swift
+//  SoberMate
+//
+//  Created by JONGHUN PARK on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SoberMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
